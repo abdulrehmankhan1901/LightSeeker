1f6fe629fc4438ea45c2c12e11ab8cb62c6b498c
abdulrehmankhan1901 <98641285+abdulrehmankhan1901@users.noreply.github.com>
2023-04-09 13:43:33 +0500

    Game Mechanics

---
+++ Source/lightseeker/Pickable.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "Pickable.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LIGHTSEEKER_API APickable : public AItem
+{
+	GENERATED_BODY()
+public:
+
+	APickable();
+
+	//inherited from Item; therefore, no need to mark these functions as UFUNCTION
+	virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+	virtual void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+};
